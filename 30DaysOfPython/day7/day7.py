1c0732d05bc2c0323ece825e5132c49ffe0d52a8
abba360 <81328045+abba360@users.noreply.github.com>
2023-01-29 16:02:48 +0100

    Upadate day7.py

--- 30DaysOfPython/day7/day7.py
+++ 30DaysOfPython/day7/day7.py
@@ -52,4 +52,3 @@ it_companies.add('Twitter')
 # Convert the ages to a set and compare the length of the list and the set, which one is bigger?
 # Explain the difference between the following data types: string, list, tuple and set
 # I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? Use the split methods and set to get the unique words.
-# 🎉 
